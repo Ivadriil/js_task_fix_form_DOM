c9246ccdc4f8b69cfd8a6c5245f55bcfebe2ee32
Ivadriil <petrivi51@gmail.com>
2026-03-14 19:25:41 +0200

    task2 solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,8 +7,7 @@ const label = document.createElement('label');
 const input = document.querySelectorAll('.field-text');
 
 input.forEach((el) => {
-  el.placeholder = (el.name).replace(/\b\w/g, (l) => l.toUpperCase());
-  el.outerHTML = `<label class="field-label" for="${el.id}">${(el.name).toUpperCase()}</label>${el.outerHTML}`;
+  el.placeholder = el.name.replace(/\b\w/g, (l) => l.toUpperCase());
+  el.outerHTML = `<label class="field-label" for="${el.id}">${el.name.toUpperCase()}</label>${el.outerHTML}`;
 });
 label.classList.add('field-label');
-console.log(label);
