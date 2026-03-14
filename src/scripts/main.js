1dc8f05d9f833248802caee0a99fcc0be9f05bbc
Ivadriil <petrivi51@gmail.com>
2026-03-14 19:22:11 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
+// placeholder=""
 // write code here
+const label = document.createElement('label');
+
+const input = document.querySelectorAll('.field-text');
+
+input.forEach((el) => {
+  el.placeholder = (el.name).replace(/\b\w/g, (l) => l.toUpperCase());
+  el.outerHTML = `<label class="field-label" for="${el.id}">${(el.name).toUpperCase()}</label>${el.outerHTML}`;
+});
+label.classList.add('field-label');
+console.log(label);
