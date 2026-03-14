776b93462252a7bd15e4c38a24801d2decbf4cdc
Ivadriil <petrivi51@gmail.com>
2026-03-14 19:38:58 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,12 +2,17 @@
 
 // placeholder=""
 // write code here
-const label = document.createElement('label');
 
+const label = document.createElement('label');
+label.classList.add('field-label');
 const input = document.querySelectorAll('.field-text');
 
+
 input.forEach((el) => {
+   
   el.placeholder = el.name.replace(/\b\w/g, (l) => l.toUpperCase());
-  el.outerHTML = `<label class="field-label" for="${el.id}">${el.name.toUpperCase()}</label>${el.outerHTML}`;
+  label.htmlFor = el.id;
+  el.parentElement.insertBefore(label.cloneNode(label.textContent = el.name), el);
+  
 });
-label.classList.add('field-label');
+
